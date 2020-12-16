c387797851f293d69989ebbda7ba9a980f6474ec
SirSimon04 <simi@engelnetz.de>
2020-12-16 01:12:16 +0100

    CupertinoTabBar

--- lib/manager.dart
+++ lib/manager.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'constants.dart';
 import 'choose_room.dart';
 import 'co2_screen.dart';
 import 'info.dart';
+import 'dart:io' show Platform;
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -20,39 +22,56 @@ class HomePageState extends State<HomePage> {
     RoomPickerWidget(),
   ];
 
+  List<BottomNavigationBarItem> bottomNavigationBarItems = [
+    BottomNavigationBarItem(
+      icon: Icon(Icons.business),
+      label: 'Raumauswahl',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.location_city),
+      label: 'Übersicht',
+      backgroundColor: kInactiveBottomItem,
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.perm_device_information_rounded),
+      label: 'Information',
+    ),
+  ];
+
+  BottomNavigationBar androidBottomTabBar() {
+    return BottomNavigationBar(
+      items: bottomNavigationBarItems,
+      backgroundColor: kRedGew,
+      currentIndex: selectedIndex,
+      fixedColor: kAccentColor,
+      unselectedItemColor: kInactiveBottomItem,
+      onTap: onItemTapped,
+    );
+  }
+
+  CupertinoTabBar iosBottomTabBar() {
+    return CupertinoTabBar(
+      items: bottomNavigationBarItems,
+      backgroundColor: kRedGew,
+      currentIndex: selectedIndex,
+      activeColor: kAccentColor,
+      inactiveColor: kInactiveBottomItem,
+      onTap: onItemTapped,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kRedGew,
-        title: Text('CO2-School'),
-      ),
-      body: Center(
-        child: widgetOptions.elementAt(selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Raumauswahl',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.location_city),
-            label: 'Übersicht',
-            backgroundColor: kInactiveBottomItem,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.perm_device_information_rounded),
-            label: 'Information',
-          ),
-        ],
-        backgroundColor: kRedGew,
-        currentIndex: selectedIndex,
-        fixedColor: kAccentColor,
-        unselectedItemColor: kInactiveBottomItem,
-        onTap: onItemTapped,
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: kRedGew,
+          title: Text('CO2-School'),
+        ),
+        body: Center(
+          child: widgetOptions.elementAt(selectedIndex),
+        ),
+        bottomNavigationBar:
+            Platform.isIOS ? iosBottomTabBar() : androidBottomTabBar());
   }
 
   void onItemTapped(int index) {
